950fb3c46ace44d971faccfdc96a2c58447bc44f
ayush300302 <ayushpatil2016@gmail.com>
2026-02-25 15:32:46 +0530

    Add AQI data analysis

--- task1.py
+++ task1.py
@@ -18,3 +18,11 @@ print("Median Temperature:", median_temp)
 print("\nHumidity Data:", humidity)
 print("Average Humidity:", avg_humidity)
 print("Median Humidity:", median_humidity)
+aqi = [82, 90, 76, 88, 95, 102, 85, 79, 91, 87]
+
+avg_aqi = sum(aqi) / len(aqi)
+median_aqi = statistics.median(aqi)
+
+print("AQI Data:", aqi)
+print("Average AQI:", avg_aqi)
+print("Median AQI:", median_aqi)
